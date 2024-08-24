a9ca711f943767524d664791513aa60a4ef621f3
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-08-24 14:08:31 +0530

    Update david.py

--- david.py
+++ david.py
@@ -1,38 +1,20 @@
-import speech_recognition as sr
 import webbrowser
 import win32com.client
 import os
 from playsound import playsound
+# from playsound import playsound
 import datetime
 from config import gemini_api_key
 import google.generativeai as genai
-import time
-
-
-conversation = []
-
-# user_history = []
-
-
-def chat(prompt):
-    global conversation
-    conversation.append({"role" : "Darshan" , "content" : prompt})
-    print(f"Role : Darshan \nContent : {prompt}")
-
-    genai.configure(api_key=gemini_api_key)
-
-    generation_config = {
-        "temperature": 1,
-        "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
-        #   "response_mime_type": "text/plain",
+@@ -27,18 +27,14 @@ def chat(prompt):
     }
 
     model = genai.GenerativeModel(
+        model_name="gemini-1.5-pro",
         model_name="gemini-1.5-flash",
         generation_config=generation_config,
-        system_instruction=f"you are jarvis, a virtual assistant. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. and if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short."
+        system_instruction=f"you are david, a virtual assistant. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. and if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short."
+        system_instruction=f"you are david, a virtual assistant, built by 'Darshan Choudhary'. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short. act friendly with user, give polite replies and act similar to humans",
     )
 
     chat_session = model.start_chat(history = [])
@@ -40,140 +22,121 @@ def chat(prompt):
     response = chat_session.send_message(prompt)
     response.resolve()
 
-    conversation.append({"role" : "Jarvis","content" : response.text})
+    conversation.append({"role" : "user" , "content" : prompt})
+    conversation.append({"role" : "david","content" : response.text})
 
-    if "history".lower() in prompt.lower():
-        return chat_session.history
-
-    if "clear conversation history".lower() in prompt.lower():
-        conversation = []
     return response.text
 
-
-def gemini_ai(prompt):
-
-    genai.configure(api_key=gemini_api_key)
-
-    generation_config = {
-        "temperature": 1,
-        "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
-        #   "response_mime_type": "text/plain",
+@@ -56,16 +52,14 @@ def gemini_ai(prompt):
     }
 
     model = genai.GenerativeModel(
         model_name="gemini-1.5-pro",
+        model_name="gemini-1.5-flash",
         generation_config=generation_config,
         system_instruction="Your name is David, an AI assistant. Act as a friendly, smart, supportive, guiding assistant to user. also remember the inputs of user. Keep your answer short, to the point. ",
     )
 
     chat = model.start_chat(history = [])
 
+
     response = chat.send_message(prompt)
 
+
     return (response.text)
-    
-def conversation_appender(prompt, response = ""):
-    conversation.append({"role" : "user" , "content" : prompt})
-    conversation.append({"role" : "david","content" : response})
 
-def classifier(command):
-    prompt = f"classify the following statement in category of 'Command' or 'question' or 'normal' and return just that one word : {command}"
-    result = gemini_ai(prompt)
-    # print(result)
-    return result.lower()
-
-def new_classifier(command):
-    prompt = f"go through {basic_category} and based on it classify the following statement in category of 'other' or 'normal' and return just that one word : {command}"
-    result = gemini_ai(prompt)
-    # print(result)
-    return result.lower()
-
-def cleaner(code_response):
-
-    '''removes python word and other symbols from code response'''
-
-    code_response1 = code_response.replace("python","")
-    final_code_response = code_response1.replace("```","")
-    return final_code_response
-def summarizer(history):
-    summary = gemini_ai(f"summarize the following text : {history}")
-    return summary
-
-def say(text):
-    speaker = win32com.client.Dispatch("SAPI.SpVoice")
-    speaker.Speak(text)
-
-def takeCommand():
-    r = sr.Recognizer()
-    # r.adjust_for_ambient_noise = 1
-
-    with sr.Microphone() as source:
-        r.pause_threshold = 0.8
-
-        audio = r.listen(source)
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language="en-us")
+@@ -120,15 +114,17 @@ def takeCommand():
 
             return query
         except Exception as e:
-            print(e)
-            say("Some error occurred by Jarvis A I")
-            
+            pass
+            # print(e)
+            say("Some error occurred by David A I")
+            # say("Some error occurred by David A I")
+
+
 basic_category = {
     "other" : ["open text file", "open specific app/file" ,"create a new text file with name", "find specific folder", "give path of specific     file"] , 
     "normal" : ["remember something" , "recall it"]}
+    "other" : ["open text file", "open specific app/file" ,"create a new text file with name", "find specific folder", "give path of specific file" , "play specific song, etc.."] , 
+    "normal" : ["remember something" , "recall it","tell me a joke"]}
 
 if __name__ == "__main__":
-    say("Hello I am Jarvis A I")
+    print("Hello I am David A I")
+    say("Hello I am David A I")
     sites = [["Youtube","https://www.youtube.com"],["wikipedia","https://www.wikipedia.org"],["google","https://google.com"]]
     while True:
-        print("listening..")
-        command = takeCommand()
-        # print(f"Query : {command}")
-        try:
-            for site in sites:
-                if f"open {site[0]}".lower() in command.lower():
-                    say(f"opening {site[0]} Sir")
-                    print(f"opening {site[0]} Sir")
-                    webbrowser.open(site[1])
+@@ -138,6 +134,8 @@ def takeCommand():
+            print(f"User : {command}")
 
-            if "quit" in command.lower():
-                break
+            if "quit" in command :
+                print("OK Thank You Sir , Have a nice day")
+                say("Ok Thank You Sir , Hve a nice day")
+                exit()
 
-            elif "open music" in command.lower():
-                musicPath = "C:/Users/acer/Downloads/sample_music.mp3"
-                # playsound(musicPath) #alternate way
-                os.system(f"start {musicPath}")
+            if command=="david":
+@@ -151,28 +149,25 @@ def takeCommand():
 
-            elif "time" in command.lower():
-                hour = datetime.datetime.now().strftime("%H")
-                mins = datetime.datetime.now().strftime("%M")
-                say(f"Current time is {hour} hours {mins} minutes")
+                new_category = new_classifier(command)
+                # print(new_category)
 
-            elif "search".lower() in command.lower():
-                base_url = "https://www.google.com/search?q="
-                search_item = command.split("search",1)[1] # splits the command once after word "search" and returns words after search
-                final_search_item = str('+'.join(search_item.split())) #separates words in search_item with +
-                final_url = base_url+final_search_item
-                webbrowser.open_new(final_url)
+                if "open music" in command :
+                    musicPath = "C:/Users/acer/Downloads/sample_music.mp3"
+                    # playsound(musicPath) #alternate way
+                    os.system(f"start {musicPath}")
 
-            elif "artificial".lower() in command.lower():
-                prompt = command.split("intelligence",1)[1]
-                response = gemini_ai(prompt)
+                elif "clear conversation history".lower() in command:
+                if "clear conversation history".lower() in command:
+                    conversation = []  
+
+                elif "conversation history".lower() in command :
+                    # print("here")
+                    print(conversation)
+
+                elif "other".lower() in new_category:
+                    new_prompt = f"just return the code which will fulfill the command , just return the code of following command and nothing else : {command} consider that the device is of Windows OS"
+                    # print("here")
+                    new_prompt = f"just return the code which will fulfill the command , just return the code of following command and nothing else , mostly use os module for it,but if not suitable then you can also use webbrowser module to open sites and consider that os module is already imported: {command} consider that the device is of Windows OS"
+                    code_response = gemini_ai(new_prompt)
+                    final_code = cleaner(code_response)
+                    conversation_appender(command,final_code)
+                    # print(final_code)
+                    exec(final_code)
+
+                else:
+                    # print("in command")
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+@@ -181,11 +176,15 @@ def takeCommand():
+            elif "question".lower() in category:
+
+                if "time" in command :
+                    hour = datetime.datetime.now().strftime("%H")
+                    mins = datetime.datetime.now().strftime("%M")
+                    hour = int(datetime.datetime.now().strftime("%H"))
+                    if hour>12:
+                        hour = hour - 12
+                    mins = int(datetime.datetime.now().strftime("%H"))
+                    print(f"Current time is {hour} hours {mins} minutes")
+                    say(f"Current time is {hour} hours {mins} minutes")
+
+                else:
+                    # print("in question")
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+@@ -199,6 +198,7 @@ def takeCommand():
                 print(response)
 
-            elif "conversation history".lower() in command.lower():
-                    chat_history = chat(command)
-
             else:
+                # print("in else")
                 chat_response = chat(command)
-                print(f"Role : Jarvis\nContent : {chat_response}")
+                conversation_appender(command,chat_response)
+                print(f"Role : David\nContent : {chat_response}")
                 say(chat_response)
-
-
+                
+                
         except Exception as e:
             # print(e)
             pass
